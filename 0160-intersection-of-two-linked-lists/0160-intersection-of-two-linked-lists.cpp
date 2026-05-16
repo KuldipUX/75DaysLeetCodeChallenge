53f4325b3b838a2d325b4dfe82ba66eaaf9594dc
KuldipUX <Kuldipsingh2311008@gmail.com>
2026-05-16 22:40:53 +0530

    Time: 60 ms (37.14%), Space: 23.9 MB (54.28%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,41 +9,45 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* tempA = headA;
-        ListNode* tempB = headB;
+        //pehle dono ki length nikal lete hai .
         int lenA = 0;
-        while(tempA!=NULL){
-            lenA++;
-            tempA=tempA->next;
-        }
         int lenB = 0;
+        //two node pointers for iterations
+        ListNode*tempA = headA;
+        ListNode*tempB = headB;
+        
+        //traverse to find length
+        while(tempA!=NULL ){
+            lenA++;
+            tempA = tempA->next;
+        }
         while(tempB!=NULL){
             lenB++;
-            tempB=tempB->next;
+            tempB = tempB->next;
         }
-
-        tempA=headA;
-        tempB=headB;
-
+        //after finding length reset the pointers node
+        tempA = headA;
+        tempB = headB;
         if(lenA>lenB){
             int diff = lenA-lenB;
-            for(int i=1; i<=diff;i++){
+            for(int i =0;i<diff;i++){
                 tempA = tempA->next;
             }
             while(tempA!=tempB){
-                tempA=tempA->next;
-                tempB=tempB->next;
+                tempA = tempA->next;
+                tempB = tempB->next;
             }
             return tempA;
         }
-         else{
+      else{  
             int diff = lenB-lenA;
-            for(int i=1; i<=diff;i++){
+            for(int i =0;i<diff;i++){
                 tempB = tempB->next;
             }
             while(tempB!=tempA){
-                tempA=tempA->next;
-                tempB=tempB->next;
+               
+                tempB = tempB->next;
+                tempA = tempA->next;
             }
             return tempB;
         }
