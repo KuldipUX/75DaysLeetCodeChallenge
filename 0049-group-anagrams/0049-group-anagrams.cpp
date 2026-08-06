3472de4075901c3e1dca3d8cc18664711f9383a0
KuldipUX <Kuldipsingh2311008@gmail.com>
2026-08-06 16:04:50 +0530

    Time: 11 ms (91.09%), Space: 26.2 MB (38.4%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string,vector<string>>mp;
-        for(string s:strs){
-            string key = s;
-            sort(key.begin(),key.end());
-            mp[key].push_back(s);
+      unordered_map<string,vector<string>>mp;
+        int n = strs.size();
+        for(int i = 0 ; i<n; i++){//for(string i:strs)
+            string j = strs[i];
+            sort(j.begin(),j.end());
+           mp[j].push_back(strs[i]);
+        };
+        vector<vector<string>>ans;
+        for(auto it : mp){
+            ans.push_back(it.second);
         }
-        vector<vector<string>>result;
-        for(auto it:mp){
-            result.push_back(it.second);
-        }
-        return result;
+        return ans;
+       
     }
 };
